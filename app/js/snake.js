e894442014a34cf188fded5551ce02d0d761cbf1
s3rbar <barsulsergij@gamil.com>
2026-05-13 17:13:00 +0300

    Add logic for snake page

--- app/js/snake.js
+++ app/js/snake.js
@@ -1,21 +1,158 @@
-function toggleRules(){
+function toggleRules() {
     document.querySelector('main').classList.toggle("translucent");
     document.querySelector('aside').classList.toggle("translucent");
     document.querySelector('.rules').classList.toggle("hide");
 }
 
-function generateSnkField(){};
+let blockSize = 30; 
+let total_row = 15;
+let total_col = 17;
+let field;
 
-function addSnake(){};
+let snakeX = blockSize * 5;
+let snakeY = blockSize * 5;
+let speedX = 0;
+let speedY = 0;
 
-function addApple(){};
+let snakeBody = []; 
+let foodX;
+let foodY;
 
-function updateSnkSize(){};
+let score = 0;
+let gameOver = false;
 
-//TODO: Movement code
+window.onload = function () {
+    field = document.querySelector("#field");
+    // Встановлюємо розміри поля
+    field.style.width = (total_col * blockSize) + "px";
+    field.style.height = (total_row * blockSize) + "px";
 
-function changeSnkDirection(){};
+    // Тепер ця функція точно існує нижче
+    placeFood();
+    
+    document.addEventListener("keyup", changeDirection);
+    
+    // Запускаємо ігровий цикл
+    setInterval(update, 1000 / 10);
 
-function updateSnkCount(){};
+    document.querySelector(".reset").onclick = () => location.reload();
+}
 
-function restartSnkGame(){};
+function update() {
+    if (gameOver) return;
+
+    field.innerHTML = ""; // Очищаємо поле перед кожним кадром
+
+    // 1. Малюємо їжу (яблуко)
+    let foodElement = document.createElement("img");
+    foodElement.src = "../../images/apple.png"; // Перевірте цей шлях!
+    foodElement.alt = "apple";
+    foodElement.className = "apple-img";
+    foodElement.style.left = foodX + "px";
+    foodElement.style.top = foodY + "px";
+    field.appendChild(foodElement);
+
+    // 2. Перевірка на поїдання
+    if (snakeX === foodX && snakeY === foodY) {
+        snakeBody.push([foodX, foodY]);
+        score++;
+        document.querySelector("h2 span").innerText = score;
+        placeFood();
+    }
+
+    // 3. Рух тіла
+    for (let i = snakeBody.length - 1; i > 0; i--) {
+        snakeBody[i] = snakeBody[i - 1];
+    }
+    if (snakeBody.length) {
+        snakeBody[0] = [snakeX, snakeY];
+    }
+
+    // 4. Оновлення позиції голови
+    snakeX += speedX * blockSize;
+    snakeY += speedY * blockSize;
+
+    // 5. Малюємо голову
+    let head = document.createElement("div");
+    head.className = "snake-head";
+    head.style.left = snakeX + "px";
+    head.style.top = snakeY + "px";
+    head.innerHTML = `<div class="snake-eye"></div><div class="snake-eye"></div>`;
+    field.appendChild(head);
+
+    // 6. Малюємо частини тіла
+    // Усередині функції update() при малюванні тіла:
+    snakeBody.forEach((part, index) => {
+        let bodyPart = document.createElement("div");
+        bodyPart.className = "snake-body";
+        bodyPart.style.left = part[0] + "px";
+        bodyPart.style.top = part[1] + "px";
+
+        // Визначаємо, де знаходиться попередній елемент (голова або інша частина тіла)
+        let prev = (index === 0) ? [snakeX, snakeY] : snakeBody[index - 1];
+
+        // Очищаємо бордери за замовчуванням
+        bodyPart.style.border = "none";
+
+        // Малюємо лінію на стику з попереднім елементом
+        if (prev[0] > part[0]) { // Рух вправо
+            bodyPart.style.borderRight = "1px solid #000";
+        } else if (prev[0] < part[0]) { // Рух вліво
+            bodyPart.style.borderLeft = "1px solid #000";
+        } else if (prev[1] > part[1]) { // Рух вниз
+            bodyPart.style.borderBottom = "1px solid #000";
+        } else if (prev[1] < part[1]) { // Рух вгору
+            bodyPart.style.borderTop = "1px solid #000";
+        }
+
+        field.appendChild(bodyPart);
+    });
+
+    // 7. Перевірка програшу
+    if (snakeX < 0 || snakeX >= total_col * blockSize || snakeY < 0 || snakeY >= total_row * blockSize) {
+        endGame();
+    }
+    for (let i = 0; i < snakeBody.length; i++) {
+        if (snakeX === snakeBody[i][0] && snakeY === snakeBody[i][1]) {
+            endGame();
+        }
+    }
+}
+
+// ОСЬ ЦЯ ФУНКЦІЯ, ЯКОЇ НЕ ВИСТАЧАЛО:
+function placeFood() {
+    // Випадкові координати, кратні blockSize
+    foodX = Math.floor(Math.random() * total_col) * blockSize;
+    foodY = Math.floor(Math.random() * total_row) * blockSize;
+
+    // Перевірка, щоб яблуко не з'явилося всередині змії
+    for (let i = 0; i < snakeBody.length; i++) {
+        if (foodX === snakeBody[i][0] && foodY === snakeBody[i][1]) {
+            placeFood();
+            return;
+        }
+    }
+}
+
+function endGame() {
+    gameOver = true;
+    congrats = document.querySelector(".gameOver");
+    congrats.textContent = "Гра закінчена!";
+    if(snakeBody.length < 254){
+        congrats.style.color = "red";
+    }else{
+        congrats.style.color = "green";
+    }
+}
+
+function changeDirection(e) {
+    if (e.code === "ArrowUp" && speedY !== 1) {
+        speedX = 0; speedY = -1;
+    } else if (e.code === "ArrowDown" && speedY !== -1) {
+        speedX = 0; speedY = 1;
+    } else if (e.code === "ArrowLeft" && speedX !== 1) {
+        speedX = -1; speedY = 0;
+    } else if (e.code === "ArrowRight" && speedX !== -1) {
+        speedX = 1; speedY = 0;
+    }
+}
